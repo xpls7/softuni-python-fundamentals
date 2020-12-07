949223fdd251e18263af00776461dd9b620f401e
xpls7 <34997296+xpls7@users.noreply.github.com>
2020-12-08 01:44:22 +0200

    Add files via upload

---
+++ ex.10.py
@@ -0,0 +1,30 @@
+quantity = int(input())
+days = int(input())
+
+total_money = 0
+spirit = 0
+
+for day in range(1, days+1):
+    if day % 11 == 0:
+        quantity += 2
+
+    if day % 2 == 0:
+        total_money += quantity * 2
+        spirit += 5
+    if day % 3 == 0:
+        total_money += (quantity * 5 + quantity * 3)
+        spirit += 13
+    if day % 5 == 0:
+        total_money += quantity * 15
+        spirit += 17
+        if day % 3 == 0:
+            spirit += 30
+    if day % 10 == 0:
+        spirit -= 20
+        total_money += 15 + 5 + 3
+        if day == days:
+            spirit -= 30
+
+print(f"Total cost: {total_money}")
+print(f"Total spirit: {spirit}")
+
